8893c95d504631866ccb327a51eec0a8da8f965d
Miguel Gadea <miguel.gadea@cesjuanpablossegundo.es>
2019-02-01 12:03:06 +0100

    Miguel Gadea Fernandez-Giro

--- Ejercicio32trimestre/src/ejercicio32trimestre/Ejercicio32trimestre.java
+++ Ejercicio32trimestre/src/ejercicio32trimestre/Ejercicio32trimestre.java
@@ -10,12 +10,83 @@ package ejercicio32trimestre;
  * @author Usuario DAM 1
  */
 public class Ejercicio32trimestre {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+    int[] num = new int[10];
+    int[] resultado = new int[10];
+    int contador = 0;
+    int cuentaArray = 0;
+    int prim = 0;
+    int prueba = 0;
+    
+    System.out.println("Introduzca 10 números");
+    //primero pedimos los números por teclado
+    for (int i = 0; i < 10; i++) {
+      num[i] = Integer.parseInt(System.console().readLine());
     }
     
-}
+    System.out.println("Índice\tValor");
+    //mostramos el array
+    for (int i = 0; i < 10; i++) {
+      System.out.println(i + "\t" + num[i]);
+    }
+    
+    //comprobamos cuales son primos y los posicionamos primero
+    for (int i = 0; i < 10; i++) {
+      do {
+        prim++;
+        prueba = num[i] % prim;
+        
+        if (prueba == 0) {
+          contador++;
+        } else {
+          //nada
+        }
+      } while (prim != num[i]); 
+      
+        if (contador == 2) {
+          resultado[cuentaArray] = num[i];
+          cuentaArray++;
+        } else {
+        //nada
+        }
+      
+      contador = 0;
+      prim = 0;
+    }//for
+    
+    //ahora metemos en el array resultante los que quedan
+    for (int i = 0; i < 10; i++) {
+      do {
+        prim++;
+        prueba = num[i] % prim;
+        
+        if (prueba == 0) {
+          contador++;
+        } else {
+          //nada
+        }
+      } while (prim != num[i]);  
+      
+        if (contador != 2) {
+        resultado[cuentaArray] = num[i];
+        cuentaArray++;
+        } else {
+        //nada
+        }
+      
+      contador = 0;
+      prim = 0;
+    }//for
+    
+    System.out.println("\nArray final");
+    
+    System.out.println("Índice\tValor");
+    
+    for (int i = 0; i < 10; i++) {
+      System.out.println(i + "\t" + resultado[i]);
+    }
+  }
+}  
+   
+    
+
